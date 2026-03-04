1a47b129814ca6e5a908382077bc8f6930d1bb3b
roshan <roshanarman149209@gmail.com>
2026-03-05 04:57:30 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from "path"
 
 export default defineConfig({
   plugins: [react()],
 
   resolve: {
     alias: {
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@landing': path.resolve(__dirname, './src/landingpage')
+      "@landing": path.resolve(__dirname, "src/landingpage"),
+      "@assets": path.resolve(__dirname, "src/assets")
     }
   }
 })
