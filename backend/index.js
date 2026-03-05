3ed0bc6830653da52bfdafb3bf141de0bcfb6119
roshan <roshanarman149209@gmail.com>
2026-03-05 17:12:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
- origin: "https://stockcrow.onrender.com",
+ origin: ["https://stockcrow-fronend-main-page1.onrender.com","https://stockcrow.onrender.com"],
  credentials: true,
  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
  allowedHeaders: ["Content-Type","Authorization"]
